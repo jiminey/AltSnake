fc45a88d4fb0598286a762ed81b3327ba86c0315
jiminey <jtwnguyen@ucdavis.edu>
2019-09-19 14:31:06 -0700

    Add limit conditions for snake

--- src/snake.ts
+++ src/snake.ts
@@ -4,7 +4,8 @@ export class Snake {
   private ctx: CanvasRenderingContext2D;
   private x: number;
   private y: number;
-  private dir: string = 'Right'; 
+  private dir: string = 'Right';
+  private prevDir: string = 'Right';
   private cellWidth: number;
   private cellHeight: number;
   private updateFrame: number = 0;
@@ -29,20 +30,40 @@ export class Snake {
       switch (event.key) {
         case "Down": // IE/Edge specific value
         case "ArrowDown":
+          
+        if (this.prevDir !== "Up"){
           this.dir = "Down"
+          this.prevDir = this.dir;
           break;
+        }
+
         case "Up": // IE/Edge specific value
         case "ArrowUp":
+    
+        if (this.prevDir !== "Down") {
           this.dir = "Up"
+          this.prevDir = this.dir;
           break;
+        }
+
         case "Left": // IE/Edge specific value
         case "ArrowLeft":
+
+        if (this.prevDir !== "Right") {
           this.dir = "Left"
+          this.prevDir = this.dir;
           break;
+        }
+
         case "Right": // IE/Edge specific value
         case "ArrowRight":
+        
+        if (this.prevDir !== "Left") {
           this.dir = "Right"
+          this.prevDir = this.dir;
           break;
+        }
+
         // case "Enter":
         //   // Do something for "enter" or "return" key press.
         //   break;
@@ -75,41 +96,45 @@ export class Snake {
     if (this.updateFrame % 10 === 0) {
       switch (this.dir) {
         case "Down":
-
-          this.tail = this.snake.shift(); //remove tail
-          this.tail[0] = this.head[0]; //reassign tail coords
-          this.tail[1] = this.head[1] + 1; 
-          this.snake.push(this.tail); //add tail to front
-          this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          if (this.prevDir !== "Up"){
+            this.tail = this.snake.shift(); //remove tail
+            this.tail[0] = this.head[0]; //reassign tail coords
+            this.tail[1] = this.head[1] + 1; 
+            this.snake.push(this.tail); //add tail to front
+            this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          }
 
           break;
         case "Up": 
-
-          this.tail = this.snake.shift(); //remove tail
-          this.tail[0] = this.head[0]; //reassign tail coords
-          this.tail[1] = this.head[1] - 1;
-          this.snake.push(this.tail); //add tail to front
-          this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          if (this.prevDir !== "Down") {          
+            this.tail = this.snake.shift(); //remove tail
+            this.tail[0] = this.head[0]; //reassign tail coords
+            this.tail[1] = this.head[1] - 1;
+            this.snake.push(this.tail); //add tail to front
+            this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          }
 
           break;
         case "Left": 
-
-          this.tail = this.snake.shift(); //remove tail
-          this.tail[0] = this.head[0] - 1; //reassign tail coords
-          this.tail[1] = this.head[1];
-          this.snake.push(this.tail); //add tail to front
-          this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          if (this.prevDir !== "Right") {
+            this.tail = this.snake.shift(); //remove tail
+            this.tail[0] = this.head[0] - 1; //reassign tail coords
+            this.tail[1] = this.head[1];
+            this.snake.push(this.tail); //add tail to front
+            this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          }
 
           break;
         case "Right": 
-
-          this.tail = this.snake.shift(); //remove tail
-          this.tail[0] = this.head[0] + 1; //reassign tail coords
-          this.tail[1] = this.head[1];
-          this.snake.push(this.tail); //add tail to front
-          this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
-
+          if (this.prevDir !== "Left") {
+            this.tail = this.snake.shift(); //remove tail
+            this.tail[0] = this.head[0] + 1; //reassign tail coords
+            this.tail[1] = this.head[1];
+            this.snake.push(this.tail); //add tail to front
+            this.head = this.snake[this.snake.length - 1]; //change head coords to the new tail/head
+          }
           break;
+
         default:
           return;
       } 
